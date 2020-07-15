b148b185a140235bba08a932d2adc1840cce79e3
Dawood Khan <dawoodkhan82@gmail.com>
2020-07-15 12:48:34 -0400

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -1,6 +1,18 @@
 import gradio
-from gpt import get_model, predict
+from gpt import get_model
 
+
+model, tokenizer = get_model()
+
+def predict(inp):
+    input_ids = tokenizer.encode(inp, return_tensors='tf')
+    beam_output = model.generate(input_ids, max_length=100, num_beams=5,
+                                 no_repeat_ngram_size=2, early_stopping=True)
+    output = tokenizer.decode(beam_output[0], skip_special_tokens=True,
+                              clean_up_tokenization_spaces=True)
+    return ".".join(output.split(".")[:-1]) + "."
+
+ 
 
 INPUTS = gradio.inputs.Textbox()
 OUTPUTS = gradio.outputs.Textbox()
