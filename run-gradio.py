ab7097a8cef50d5af0e52565c27204ade973658d
Dawood Khan <dawoodkhan82@gmail.com>
2020-07-15 12:24:32 -0400

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -4,7 +4,9 @@ from gpt import get_model, predict
 
 INPUTS = gradio.inputs.Textbox()
 OUTPUTS = gradio.outputs.Textbox()
-INTERFACE = gradio.Interface(fn=predict, inputs=INPUTS, outputs=OUTPUTS,
+INTERFACE = gradio.Interface(fn=predict, inputs=INPUTS, outputs=OUTPUTS, title="GPT-2",
+                 description="GPT-2 is a large transformer-based language model with 1.5 billion parameters, trained on a dataset[1] of 8 million web pages. GPT-2 is trained with a simple objective: predict the next word, given all of the previous words within some text.",
+                 thumbnail="https://github.com/gradio-app/gpt-2/raw/master/screenshots/interface.png?raw=true",
                              load_fn=get_model, capture_session=False)
 
 INTERFACE.launch(inbrowser=True)
